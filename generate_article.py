aadc2534a4e18653cc62de16ab4f9d44ecb2384a
alexandreN7 <prxo@live.fr>
2020-03-14 12:43:42 +0100

    Ajout de revue_black_ink.txt + Correction review_black_ink.txt

--- generate_article.py
+++ generate_article.py
@@ -21,7 +21,8 @@ article_list_en = [
 article_list_fr = [
 "revue_kuretake_gansai_tambi",
 "revue_fountain_pen_lamy2000",
-"revue_portable_painter"
+"revue_portable_painter",
+"revue_black_ink"
 ]
 
 
