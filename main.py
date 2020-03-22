553a8fb451288031b0827d0e115d8f5998d2147c
alexandreN7 <prxo@live.fr>
2020-03-22 17:58:23 +0100

    Mise à jour de la gestion de la galerie pictures

--- main.py
+++ main.py
@@ -47,12 +47,264 @@ def about():
     return render_template("about.html")
 
 @app.route("/galerie_peinture")
-def galery():
+def gallery():
     return render_template("galerie_peinture.html")
-    
+   
 @app.route("/galerie_pictures")
-def galery_picture():
-    return render_template("galerie_pictures.html")
+def gallery_pictures():
+    return render_template("galerie_pictures.html") 
+
+@app.route("/sheet_bee-4944489_960_720")
+def test_bee_4944489_960_720():
+    return render_template("sheet_bee-4944489_960_720.html")
+
+@app.route("/sheet_bee-4944485_960_720")
+def test_bee_4944485_960_720():
+    return render_template("sheet_bee-4944485_960_720.html")
+
+@app.route("/sheet_bee-4944486_960_720")
+def test_bee_4944486_960_720():
+    return render_template("sheet_bee-4944486_960_720.html")
+
+@app.route("/sheet_butterfly-4951289_960_720")
+def test_butterfly_4951289_960_720():
+    return render_template("sheet_butterfly-4951289_960_720.html")
+
+@app.route("/sheet_bulrush-4951322_960_720")
+def test_bulrush_4951322_960_720():
+    return render_template("sheet_bulrush-4951322_960_720.html")
+
+@app.route("/sheet_butterfly-4951309_960_720")
+def test_butterfly_4951309_960_720():
+    return render_template("sheet_butterfly-4951309_960_720.html")
+
+@app.route("/sheet_butterfly-4951310_960_720")
+def test_butterfly_4951310_960_720():
+    return render_template("sheet_butterfly-4951310_960_720.html")
+
+@app.route("/sheet_flowers-4951325_960_720")
+def test_flowers_4951325_960_720():
+    return render_template("sheet_flowers-4951325_960_720.html")
+
+@app.route("/sheet_bee-4944487_960_720")
+def test_bee_4944487_960_720():
+    return render_template("sheet_bee-4944487_960_720.html")
+
+@app.route("/sheet_flowers-4951286_960_720")
+def test_flowers_4951286_960_720():
+    return render_template("sheet_flowers-4951286_960_720.html")
+
+@app.route("/sheet_flowers-4951332_960_720")
+def test_flowers_4951332_960_720():
+    return render_template("sheet_flowers-4951332_960_720.html")
+
+@app.route("/sheet_bird-4930603_960_720")
+def test_bird_4930603_960_720():
+    return render_template("sheet_bird-4930603_960_720.html")
+
+@app.route("/sheet_duck-4926219_960_720")
+def test_duck_4926219_960_720():
+    return render_template("sheet_duck-4926219_960_720.html")
+
+@app.route("/sheet_lizard-4941499_960_720")
+def test_lizard_4941499_960_720():
+    return render_template("sheet_lizard-4941499_960_720.html")
+
+@app.route("/sheet_flower-4944491_960_720")
+def test_flower_4944491_960_720():
+    return render_template("sheet_flower-4944491_960_720.html")
+
+@app.route("/sheet_lizard-4941507_960_720")
+def test_lizard_4941507_960_720():
+    return render_template("sheet_lizard-4941507_960_720.html")
+
+@app.route("/sheet_moutain-4941506_960_720")
+def test_moutain_4941506_960_720():
+    return render_template("sheet_moutain-4941506_960_720.html")
+
+@app.route("/sheet_lizard-4941501_960_720")
+def test_lizard_4941501_960_720():
+    return render_template("sheet_lizard-4941501_960_720.html")
+
+@app.route("/sheet_mushrooms-4873141_960_720")
+def test_mushrooms_4873141_960_720():
+    return render_template("sheet_mushrooms-4873141_960_720.html")
+
+@app.route("/sheet_leaves-4873125_960_720")
+def test_leaves_4873125_960_720():
+    return render_template("sheet_leaves-4873125_960_720.html")
+
+@app.route("/sheet_moutain-4941509_960_720")
+def test_moutain_4941509_960_720():
+    return render_template("sheet_moutain-4941509_960_720.html")
+
+@app.route("/sheet_duck-4873132_960_720")
+def test_duck_4873132_960_720():
+    return render_template("sheet_duck-4873132_960_720.html")
+
+@app.route("/sheet_goose-4873142_960_720")
+def test_goose_4873142_960_720():
+    return render_template("sheet_goose-4873142_960_720.html")
+
+@app.route("/sheet_tulip-4879970_960_720")
+def test_tulip_4879970_960_720():
+    return render_template("sheet_tulip-4879970_960_720.html")
+
+@app.route("/sheet_flowers-4873131_960_720")
+def test_flowers_4873131_960_720():
+    return render_template("sheet_flowers-4873131_960_720.html")
+
+@app.route("/sheet_cat-4774164_960_720")
+def test_cat_4774164_960_720():
+    return render_template("sheet_cat-4774164_960_720.html")
+
+@app.route("/sheet_versailles-4873133_960_720")
+def test_versailles_4873133_960_720():
+    return render_template("sheet_versailles-4873133_960_720.html")
+
+@app.route("/sheet_tulip-4879969_960_720")
+def test_tulip_4879969_960_720():
+    return render_template("sheet_tulip-4879969_960_720.html")
+
+@app.route("/sheet_mushroom-4686877_960_720")
+def test_mushroom_4686877_960_720():
+    return render_template("sheet_mushroom-4686877_960_720.html")
+
+@app.route("/sheet_mushroom-4693264_960_720")
+def test_mushroom_4693264_960_720():
+    return render_template("sheet_mushroom-4693264_960_720.html")
+
+@app.route("/sheet_mushroom-4655464_960_720")
+def test_mushroom_4655464_960_720():
+    return render_template("sheet_mushroom-4655464_960_720.html")
+
+@app.route("/sheet_flowers-4873127_960_720")
+def test_flowers_4873127_960_720():
+    return render_template("sheet_flowers-4873127_960_720.html")
+
+@app.route("/sheet_mushrooms-4655480_960_720")
+def test_mushrooms_4655480_960_720():
+    return render_template("sheet_mushrooms-4655480_960_720.html")
+
+@app.route("/sheet_sun-4731859_960_720")
+def test_sun_4731859_960_720():
+    return render_template("sheet_sun-4731859_960_720.html")
+
+@app.route("/sheet_mushroom-4655463_960_720")
+def test_mushroom_4655463_960_720():
+    return render_template("sheet_mushroom-4655463_960_720.html")
+
+@app.route("/sheet_mushrooms-4693269_960_720")
+def test_mushrooms_4693269_960_720():
+    return render_template("sheet_mushrooms-4693269_960_720.html")
+
+@app.route("/sheet_mushroom-4655479_960_720")
+def test_mushroom_4655479_960_720():
+    return render_template("sheet_mushroom-4655479_960_720.html")
+
+@app.route("/sheet_hephaistos-temple-4673515_960_720")
+def test_hephaistos_temple_4673515_960_720():
+    return render_template("sheet_hephaistos-temple-4673515_960_720.html")
+
+@app.route("/sheet_mushroom-4655429_960_720")
+def test_mushroom_4655429_960_720():
+    return render_template("sheet_mushroom-4655429_960_720.html")
+
+@app.route("/sheet_marigold-4655440_960_720")
+def test_marigold_4655440_960_720():
+    return render_template("sheet_marigold-4655440_960_720.html")
+
+@app.route("/sheet_passionflower-4774160_960_720")
+def test_passionflower_4774160_960_720():
+    return render_template("sheet_passionflower-4774160_960_720.html")
+
+@app.route("/sheet_marigolds-4655449_960_720")
+def test_marigolds_4655449_960_720():
+    return render_template("sheet_marigolds-4655449_960_720.html")
+
+@app.route("/sheet_mushrooms-4663017_960_720")
+def test_mushrooms_4663017_960_720():
+    return render_template("sheet_mushrooms-4663017_960_720.html")
+
+@app.route("/sheet_mushroom-4655423_960_720")
+def test_mushroom_4655423_960_720():
+    return render_template("sheet_mushroom-4655423_960_720.html")
+
+@app.route("/sheet_moss-4693294_960_720")
+def test_moss_4693294_960_720():
+    return render_template("sheet_moss-4693294_960_720.html")
+
+@app.route("/sheet_flowers-4873128_960_720")
+def test_flowers_4873128_960_720():
+    return render_template("sheet_flowers-4873128_960_720.html")
+
+@app.route("/sheet_sun-4673483_960_720")
+def test_sun_4673483_960_720():
+    return render_template("sheet_sun-4673483_960_720.html")
+
+@app.route("/sheet_cat-4662991_960_720")
+def test_cat_4662991_960_720():
+    return render_template("sheet_cat-4662991_960_720.html")
+
+@app.route("/sheet_parakeet-4662983_960_720")
+def test_parakeet_4662983_960_720():
+    return render_template("sheet_parakeet-4662983_960_720.html")
+
+@app.route("/sheet_mushroom-4655468_960_720")
+def test_mushroom_4655468_960_720():
+    return render_template("sheet_mushroom-4655468_960_720.html")
+
+@app.route("/sheet_mushroom-4655434_960_720")
+def test_mushroom_4655434_960_720():
+    return render_template("sheet_mushroom-4655434_960_720.html")
+
+@app.route("/sheet_cat-4663020_960_720")
+def test_cat_4663020_960_720():
+    return render_template("sheet_cat-4663020_960_720.html")
+
+@app.route("/sheet_bird-4731870_960_720")
+def test_bird_4731870_960_720():
+    return render_template("sheet_bird-4731870_960_720.html")
+
+@app.route("/sheet_art-4673516_960_720")
+def test_art_4673516_960_720():
+    return render_template("sheet_art-4673516_960_720.html")
+
+@app.route("/sheet_nature-4673503_960_720")
+def test_nature_4673503_960_720():
+    return render_template("sheet_nature-4673503_960_720.html")
+
+@app.route("/sheet_mushroom-4673519_960_720")
+def test_mushroom_4673519_960_720():
+    return render_template("sheet_mushroom-4673519_960_720.html")
+
+@app.route("/sheet_ancient-agora-4662993_960_720")
+def test_ancient_agora_4662993_960_720():
+    return render_template("sheet_ancient-agora-4662993_960_720.html")
+
+@app.route("/sheet_mushroom-4673410_960_720")
+def test_mushroom_4673410_960_720():
+    return render_template("sheet_mushroom-4673410_960_720.html")
+
+@app.route("/sheet_nature-4673499_960_720")
+def test_nature_4673499_960_720():
+    return render_template("sheet_nature-4673499_960_720.html")
+
+@app.route("/sheet_mushroom-4662970_960_720")
+def test_mushroom_4662970_960_720():
+    return render_template("sheet_mushroom-4662970_960_720.html")
+
+@app.route("/sheet_mountains-4941503_960_720")
+def test_mountains_4941503_960_720():
+    return render_template("sheet_mountains-4941503_960_720.html")
+
+@app.route("/sheet_caterpillar-4941500_960_720")
+def test_caterpillar_4941500_960_720():
+    return render_template("sheet_caterpillar-4941500_960_720.html")
+
+@app.route("/sheet_flower-4944492_960_720")
+def test_flower_4944492_960_720():
+    return render_template("sheet_flower-4944492_960_720.html")
     
 @app.route("/sitemap")
 def sitemap():
--- static/css/layouts/autogenerate.pl
+++ static/css/layouts/autogenerate.pl
@@ -1,60 +1,3 @@
-#     <input class="gallery__select"  type="radio" name="Bees" id="0"/> 
-#     <input class="gallery__select"  type="radio" name="Bees" id="1"/>
-#     <input class="gallery__select"  type="radio" name="Bees" id="2"/>
-#     <input class="gallery__select"  type="radio" name="Butterfly" id="3"/>
-#     <input class="gallery__select"  type="radio" name="Jonc" id="4"/>
-#     <input class="gallery__select"  type="radio" name="Butterfly" id="5"/>
-#     <input class="gallery__select"  type="radio" name="Jonc" id="6"/>
-#     <input class="gallery__select"  type="radio" name="Jonc" id="7"/>
-#     <input class="gallery__select"  type="radio" name="Butterfly" id="8"/>
-#     <input class="gallery__select"  type="radio" name="Jonc" id="9"/>
-# 
-#     <div class="gallery">
-#         <label class="gallery__item" for="0"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944489_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944489_960_720.jpg" alt="Abeille Bee Prunus Alpes"></a>
-#         </label>
-#         <label class="gallery__item" for="1"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/18/15/36/bee-4944485_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/18/15/36/bee-4944485_960_720.jpg" alt="Abeille Bee Prunus Alpes"></a>
-#         </label>
-#         <label class="gallery__item" for="2"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/18/15/36/bee-4944486_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/18/15/36/bee-4944486_960_720.jpg" alt="Abeille Bee Prunus Alpes"></a>
-#         </label>
-#         <label class="gallery__item" for="3"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/20/17/07/butterfly-4951289_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/20/17/07/butterfly-4951289_960_720.jpg" alt="Butterfly Papillon Joncquille Alpes"></a>
-#         </label>
-#         <label class="gallery__item" for="4"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/20/17/15/bulrush-4951322_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/20/17/15/bulrush-4951322_960_720.jpg" alt="Jonc Alpes"></a>
-#         </label> 
-#         <label class="gallery__item" for="5"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/20/17/10/butterfly-4951309_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/20/17/10/butterfly-4951309_960_720.jpg" alt="Butterfly Papillon Joncquille Alpes"></a>
-#         </label> 
-#         <label class="gallery__item" for="6"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/20/17/10/butterfly-4951310_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/20/17/10/butterfly-4951310_960_720.jpg" alt="Butterfly Papillon Joncquille Alpes"></a>
-#         </label> 
-#         
-#         <label class="gallery__item" for="7"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/20/17/16/flowers-4951325_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/20/17/16/flowers-4951325_960_720.jpg" alt="Fleurs Violettes Alpes"></a>
-#         </label> 
-#         <label class="gallery__item" for="8"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944487_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944487_960_720.jpg" alt="Abeille Bee Prunus Alpes"></a>
-#         </label>
-#         <label class="gallery__item" for="9"> 
-#             <a href="https://cdn.pixabay.com/photo/2020/03/20/17/04/flowers-4951286_960_720.jpg">
-#             <img src="https://cdn.pixabay.com/photo/2020/03/20/17/04/flowers-4951286_960_720.jpg" alt="Fleurs Rouge Alpes"></a>
-#         </label> 
-
-
-
-
 my @tab = ();
 push @tab, "https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944489_960_720.jpg";
 push @tab, "https://cdn.pixabay.com/photo/2020/03/18/15/36/bee-4944485_960_720.jpg";
@@ -66,7 +9,6 @@ push @tab, "https://cdn.pixabay.com/photo/2020/03/20/17/10/butterfly-4951310_960
 push @tab, "https://cdn.pixabay.com/photo/2020/03/20/17/16/flowers-4951325_960_720.jpg";
 push @tab, "https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944487_960_720.jpg";
 push @tab, "https://cdn.pixabay.com/photo/2020/03/20/17/04/flowers-4951286_960_720.jpg";
-push @tab, "https://cdn.pixabay.com/photo/2020/03/18/15/37/bee-4944487_960_720.jpg";
 push @tab, "https://cdn.pixabay.com/photo/2020/03/20/17/20/flowers-4951332_960_720.jpg"; 
 push @tab, "https://cdn.pixabay.com/photo/2020/03/14/13/10/bird-4930603_960_720.jpg";
 push @tab, "https://cdn.pixabay.com/photo/2020/03/12/20/49/duck-4926219_960_720.jpg";
@@ -132,7 +74,6 @@ push @desc, "Butterfly Papillon Joncquille Jaune Alpes Alps";
 push @desc, "Fleurs Flowers Violette Purple Alps Alpes";
 push @desc, "Bee Abeille Prunus Alpes Alps";
 push @desc, "Fleurs Flowers Red Rouge Alps Alpes";
-push @desc, "Bee Abeille Prunus Alpes Alps";
 push @desc, "Fleurs Flowers Violette Purple Alps Alpes";
 push @desc, "Oiseau Rouge-Gorge Bird Robin Red";
 push @desc, "Canard Duck Mallard Col-Vert Bird Oiseau";
@@ -187,23 +128,88 @@ push @desc, "Mountain Montagne Chemin des Aronards Ponsonnas Alpes Alps";
 push @desc, "Chenille Caterpillar Animal";
 push @desc, "Fleur Violette Purple Flower Macro";
 
-open (FILE, ">htmlfile.txt");
+my @path_tab = split (" ", `pwd`);
+my $path = $path_tab[0];
+$path =~ s/pause-nature\/pause-nature\/(.*)?/pause-nature\/pause-nature/g;
+my $templatePath = $path."/templates";
+my $cssPath = $path."/static/css/layouts";
+
+######################################
+### HTML part 
+### To copy in HTML file 
+######################################
+open (FILE_main, ">$templatePath/copyToMainPy.txt");
+open (FILE, ">$templatePath/htmlfile.txt");
 for (0..scalar(@tab)-1)
 {
      print FILE "     <input class=\"gallery__select\"  type=\"radio\" name=\"autogenerated\" id=\"".$_."\"/>\n";
 }
 print FILE "\n";
 print FILE "\n";
+print FILE "    <div class=\"gallery\">\n";
 for (0..scalar(@tab)-1)
 {
-print FILE "         <label class=\"gallery__item\" for=\"".$_."\">\n"; 
-print FILE "             <a href=\"".$tab[$_]."\">\n";
-print FILE "             <img src=\"".$tab[$_]."\" alt=\"".$desc[$_]."\"></a>\n";
-print FILE "         </label>\n";
+    my $name = $tab[$_];
+    $name =~ s/(.*)?\///g;
+    $name =~ s/.jpg//g;
+    print FILE "         <label class=\"gallery__item\" for=\"".$_."\">\n"; 
+    print FILE "             <a href=\"/sheet_$name\" target=\"_blank\">\n";
+    print FILE "             <img src=\"".$tab[$_]."\" alt=\"".$desc[$_]."\"></a>\n";
+    print FILE "         </label>\n";
+    
+    ### main.py
+    autoMain($name);
+    
+    #### Generate file HTML for picture
+    autoHTML($name, $_);
 }
+print FILE "    </div> <!-- End div classe gallery -->\n";
 close(FILE);
+close(FILE_main);
 
-open (FILE, ">cssfile.txt");
+######################################
+### auto generate CSS
+######################################
+my $nbrLines = int(scalar(@tab)/4)+1;
+open (FILE, ">$cssPath/gallery_picture1.css");
+print FILE "* {
+  box-sizing: border-box;
+  -webkit-animation: fadeIn 0.5s;
+          animation: fadeIn 0.5s;
+}
+img {
+  height: 100%;
+  width: 100%;
+  min-height: 50px;
+  -o-object-fit: cover;
+     object-fit: cover;
+}
+
+.gallery {
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(4, 300px);
+  grid-gap: 20px;
+  grid-template-rows: repeat($nbrLines, 300px);
+}
+
+.gallery__item {
+  cursor: pointer;
+  border-radius: 5px;
+  grid-row: span 1;
+  grid-column: span 1;
+  transition: -webkit-transform 0.1s ease-in-out;
+  transition: transform 0.1s ease-in-out;
+  transition: transform 0.1s ease-in-out, -webkit-transform 0.1s ease-in-out;
+}
+.gallery__item:hover {
+  -webkit-transform: scale(1.1) rotate(5deg);
+          transform: scale(1.1) rotate(5deg);
+}
+.gallery__select {
+  display: none;
+}\n";
+
 for (0..scalar(@tab)-1)
 {
     my $nbr = $_+1;
@@ -225,4 +231,128 @@ for (0..scalar(@tab)-1)
     print FILE "  }\n";
     print FILE "}\n";
 }
+print FILE "\@-webkit-keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+\@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}\n";
 close(FILE);
+
+######################################
+### autogenerate sheets for pictures (to add in main.py
+######################################
+sub autoMain 
+{
+    my ($name) = @_;
+    my $testName = $name;
+    $testName =~ s/\-/\_/g;
+    ### file main.py
+    print FILE_main "\@app.route(\"/sheet_$name\")
+def test_$testName():
+    return render_template(\"sheet_$name.html\")\n\n";
+}
+
+sub autoHTML
+{
+    my ($name, $index) = @_;
+    open (FILE_sheet, ">$templatePath/sheet_$name.html");
+        print FILE_sheet "<!doctype html>
+<html lang=\"en\">
+    <head>
+
+        <script data-ad-client=\"ca-pub-4353004932795007\" async
+                src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+
+        <meta name=\"google-site-verification\" content=\"9cZqCjgkf5l9r1cUe7kBRjXSj4Ru49PHJiXJmp96S7k\" />
+        <meta name=\"msvalidate.01\" content=\"ADBE1D6C621C0EE7980A84478EED53ED\" />
+        <meta charset=\"utf-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <link rel=\"stylesheet\" href=\"../../static/css/layouts/pure-min.css\">
+        <link rel=\"stylesheet\" href=\"../../static/css/layouts/grids-responsive-min.css\">
+        <link rel=\"stylesheet\" href=\"../../static/css/layouts/blog.css\">
+
+        <script type=\"application/ld+json\">{\"\@context\" : \"http://schema.org\",\"\@type\" : \"Article\",\"name\" : \"pictures\",\"author\" : {\"\@type\" : \"Person\",\"name\" : \"Amandine Poterala\"},\"articleSection\" : \"Pictures\"}</script>
+        <meta name=\"description\" content=\"Amandine's pictures.\">    
+
+    </head>
+    
+    <body>
+       <div id=\"layout\" class=\"pure-g\">
+            <div class=\"sidebar pure-u-1 pure-u-md-1-5\">
+                <div class=\"header\">
+
+                    <p><center>
+                        <img width=\"140\" height=\"140\" alt=\"Alexandre Proux avatar\"
+
+src=\"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/54851225-13ed-4999-be28-883e143e461c/ddp24an-71c94052-a418-4980-b211-0397c9e238c7.jpg/v1/fill/w_894,h_894,q_70,strp/painted_for_a_friend_by_archiwyzard_ddp24an-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTYwMCIsInBhdGgiOiJcL2ZcLzU0ODUxMjI1LTEzZWQtNDk5OS1iZTI4LTg4M2UxNDNlNDYxY1wvZGRwMjRhbi03MWM5NDA1Mi1hNDE4LTQ5ODAtYjIxMS0wMzk3YzllMjM4YzcuanBnIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.llFxrx1UaPqLZcz3wlU3Cf-NzxRN0M3iDrbx-z0jI04\">
+                    </center></p>
+
+                    <h1 class=\"brand-title\">Pause nature</h1>
+                    <h2 class=\"brand-tagline\">Nature, watercolor and photography</h2>
+                    <h2 class=\"brand-tagline\"></h2>
+                    <nav class=\"nav\">
+                        <ul class=\"nav-list\">
+                            <li class=\"nav-item\">
+                                <a class=\"pure-button\" href=\"/home\">Home</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"pure-button\" href=\"/reviews\">Reviews</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"pure-button\" href=\"/galerie_peinture\">Paintings</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"pure-button\" href=\"/galerie_pictures\">Pictures</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"pure-button\" href=\"/about\">About</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <p><center>
+                        <a href=\"https://www.instagram.com/archiwyzard/\"><img src=\"../../static/img/common/logo_instagram.jpg\" alt=\"logo instagram\" height=\"30px\" width=\"30px\" /></a>
+                        <a href=\"https://www.deviantart.com/archiwyzard/\"><img src=\"../../static/img/common/logo_deviantart.jpeg\" alt=\"logo deviantart\" height=\"30px\" width=\"30px\" /></a>
+                    <center></p>
+                </div>
+            </div>
+            
+            
+<div class=\"content pure-u-1 pure-u-md-3-4\">
+
+    <header class=\"ScriptHeader\">
+            <div class=\"col-rt-12\">
+                    <div class=\"rt-heading\">
+                    <h1>Bee Abeille Prunus Alpes Alps</h1>
+                </div>
+            </div>
+    </header>
+    
+    <center>
+    <img src=\"$tab[$index]\" alt=\"$desc[$index]\">
+    </center>
+    
+<div> <!-- End div pure blabla -->
+            
+                        <div class=\"footer\">
+                            <p>Copyright &copy; Alexandre Proux </p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>\n";
+    
+    close(FILE_sheet);
+}
