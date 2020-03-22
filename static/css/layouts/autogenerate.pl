427442c021391824b940d206b41c2c34b931f4c3
alexandreN7 <prxo@live.fr>
2020-03-22 19:52:16 +0100

    Tentative adaptation portable

--- static/css/layouts/autogenerate.pl
+++ static/css/layouts/autogenerate.pl
@@ -146,7 +146,7 @@ for (0..scalar(@tab)-1)
 }
 print FILE "\n";
 print FILE "\n";
-print FILE "    <div class=\"gallery content pure-u-1 pure-u-md-1-2 pure-u-lg-1-3\">\n";
+print FILE "    <div class=\"gallery\">\n";
 for (0..scalar(@tab)-1)
 {
     my $name = $tab[$_];
@@ -154,7 +154,7 @@ for (0..scalar(@tab)-1)
     $name =~ s/.jpg//g;
     print FILE "         <label class=\"gallery__item\" for=\"".$_."\">\n"; 
     print FILE "             <a href=\"/sheet_$name\" target=\"_blank\">\n";
-    print FILE "             <img class=\"cover\" height=100% width=100% min-height=\"50px\" src=\"".$tab[$_]."\" alt=\"".$desc[$_]."\"></a>\n";
+    print FILE "             <img class=\"cover pure-u-1 pure-u-md-1-1 pure-u-lg-1-1\" height=100% width=100% min-height=\"50px\" src=\"".$tab[$_]."\" alt=\"".$desc[$_]."\"></a>\n";
     print FILE "         </label>\n";
     
     ### main.py
@@ -334,8 +334,8 @@ src=\"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/54851225-13ed-49
             </div>
     </header>
     
-    <div class=\"photo-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-3\">
-    <img src=\"$tab[$index]\" alt=\"$desc[$index]\">
+    <div class=\"photo-box pure-u-1 pure-u-md-1-1 pure-u-lg-1-1\">
+    <img width=100% height=100% src=\"$tab[$index]\" alt=\"$desc[$index]\">
     </div>
     
 <div> <!-- End div pure blabla -->
