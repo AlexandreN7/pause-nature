b1f22aeef6c774b4c9bde929243e7b9c2c7b8750
APO <poterala21@gmail.com>
2021-09-04 17:24:25 +0200

    Déplacement dans un script de la page reviews

--- generate_article.py
+++ generate_article.py
@@ -5,6 +5,7 @@ import os
 import re 
 import generate_pictures
 import generate_paintings
+import generate_reviews
 
 #site_configuration = open("site_conf.txt","r")
 
@@ -269,35 +270,6 @@ template.close()
 
 #generation de la table des article
 
-#generation de de l'onglet reviews EN
-review_html=open("templates/reviews.html","w")
-print("Generation de l'index :")
-template = open("templates/template_fr.html", "r")
-for ligne in template :
-   if "</head>" in ligne : #insertion du <head>
-      review_html.write('<meta name="description" content="Cette page contient la liste des articles publiés sur les thêmes de la randonnée, du dessin et de la peinture aquarelle">  ')#du titre
-      review_html.write('<title>Archive des articles</title>')#du titre
-      review_html.write('<link rel="alternate" hreflang="en" href="http://www.pause-nature.fr/reviews">')
-      review_html.write('<link rel="shortcut icon" type="image/ico" href="/static/img/favicon.ico">')
-
-   if "content_beginning" in ligne : #debut index
-      review_html.write('<div class="content pure-u-1 pure-u-md-3-4">')
-
-      for article in article_list_fr :
-         review_html.write('<a href=/fr/revue/'+article+' >')
-         review_html.write('<p>'+article+'</p>')
-         review_html.write('</a>')
-      review_html.write('<br>')
-      review_html.write('<br>')
-      for article in article_list_en :
-         review_html.write('<a href=/en/review/'+article+' >')
-         review_html.write('<p>'+article+'</p>')
-         review_html.write('</a>')
-
-
-
-   review_html.write(ligne)#ecriture des lignes du templates
-review_html.close()
-template.close()
+generate_reviews.gen_reviews_page(article_list_en, article_list_fr)
 
 
